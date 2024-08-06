a59c270c6bce525e65bb213370ef5f60699515bc
Abhinav Arora <abhinavarora310@gmail.com>
2024-08-06 22:03:59 +0530

    init

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,31 +3,10 @@ const Home = ()=>{
         <div className="main">
             <div className="wrapper">
                 <div className="details-container">
-                    <p>Burger King</p>
-                    <img src="/img/burgerKing_landingPage.png" alt="" />
-                    <button onClick={()=>{
-                        window.location.href = 'https://www.figma.com/proto/nw0KcoIp9eWgSxTy7Zy2fR/PlanCover?page-id=410%3A18&node-id=470-3&viewport=-121%2C-348%2C0.68&t=aKq5uQTyq9NPsNUk-1&scaling=min-zoom&content-scaling=fixed'
-                    }}>Live Demo</button>
-                </div>
-                <div className="details-container coffee">
-                    <p>Coffee App</p>
-                    <img src="/img/coffee_app.png" alt=""  />
-                    <button onClick={()=>{
-                        window.location.href = 'https://www.figma.com/proto/1rG5SOpAPaVk3wZT0Vwpmd/Untitled?page-id=196%3A522&node-id=196-523&viewport=767%2C417%2C0.72&t=rOOHDHkdrv7ntgBS-1&scaling=min-zoom&content-scaling=fixed'
-                    }}>Live Demo</button>
-                </div>
-                <div className="details-container">
                     <p></p>
                     <img src="/img/HRMS_prototype.png" alt="" />
                     <button onClick={()=>{
                         window.location.href = 'https://www.figma.com/proto/bNg3fDkuyiBeMFcuCUcN8M/Portfolio?page-id=429%3A2&node-id=431-16037&viewport=2496%2C292%2C0.24&t=aqNVXMcPbR9HBbgG-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=431%3A16037'
-                    }}>Live Demo</button>
-                </div>
-                <div className="details-container">
-                    <p></p>
-                    <img src="/img/Imagen_landingPage.png" alt="" />
-                    <button onClick={()=>{
-                        window.location.href = 'https://www.figma.com/proto/ryn0xjKHHf1Xyq3dZPAGxc/website_banner?page-id=0%3A1&node-id=3-2&t=ldA8beE1v7mVq7ck-1'
                     }}>Live Demo</button>
                 </div>
                 <div className="details-container">
@@ -46,6 +25,31 @@ const Home = ()=>{
                     window.location.href = 'https://www.figma.com/proto/1rG5SOpAPaVk3wZT0Vwpmd/Untitled?page-id=196%3A325&node-id=196-326&viewport=819%2C423%2C0.46&t=nM0DzVqLzNikImeI-1&scaling=min-zoom&content-scaling=fixed'
                     }}>Live Demo</button>
                 </div>
+                <div className="details-container coffee">
+                    <p>Coffee App</p>
+                    <img src="/img/coffee_app.png" alt=""  />
+                    <button onClick={()=>{
+                        window.location.href = 'https://www.figma.com/proto/1rG5SOpAPaVk3wZT0Vwpmd/Untitled?page-id=196%3A522&node-id=196-523&viewport=767%2C417%2C0.72&t=rOOHDHkdrv7ntgBS-1&scaling=min-zoom&content-scaling=fixed'
+                    }}>Live Demo</button>
+                </div>
+                <div className="details-container">
+                    <p></p>
+                    <img src="/img/Imagen_landingPage.png" alt="" />
+                    <button onClick={()=>{
+                        window.location.href = 'https://www.figma.com/proto/ryn0xjKHHf1Xyq3dZPAGxc/website_banner?page-id=0%3A1&node-id=3-2&t=ldA8beE1v7mVq7ck-1'
+                    }}>Live Demo</button>
+                </div>
+                <div className="details-container">
+                    <p>Burger King</p>
+                    <img src="/img/burgerKing_landingPage.png" alt="" />
+                    <button onClick={()=>{
+                        window.location.href = 'https://www.figma.com/proto/nw0KcoIp9eWgSxTy7Zy2fR/PlanCover?page-id=410%3A18&node-id=470-3&viewport=-121%2C-348%2C0.68&t=aKq5uQTyq9NPsNUk-1&scaling=min-zoom&content-scaling=fixed'
+                    }}>Live Demo</button>
+                </div>
+                
+               
+               
+                
             </div>
         </div>
     )
