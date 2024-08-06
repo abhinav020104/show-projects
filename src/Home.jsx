fd7cae4e547f00bdf695f05a9b1282a2d826b5a2
Abhinav Arora <abhinavarora310@gmail.com>
2024-08-06 22:05:42 +0530

    init

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,14 @@ const Home = ()=>{
         <div className="main">
             <div className="wrapper">
                 <div className="details-container">
-                    <p></p>
+                    <p>HRMS</p>
                     <img src="/img/HRMS_prototype.png" alt="" />
                     <button onClick={()=>{
                         window.location.href = 'https://www.figma.com/proto/bNg3fDkuyiBeMFcuCUcN8M/Portfolio?page-id=429%3A2&node-id=431-16037&viewport=2496%2C292%2C0.24&t=aqNVXMcPbR9HBbgG-1&scaling=scale-down-width&content-scaling=fixed&starting-point-node-id=431%3A16037'
                     }}>Live Demo</button>
                 </div>
                 <div className="details-container">
-                    <p></p>
+                    <p>Plant Website</p>
                     <img src="/img/Plant_website.png" alt="" />
                     <button onClick={()=>{
                         
@@ -18,7 +18,7 @@ const Home = ()=>{
                     }}>Live Demo</button>
                 </div>
                 <div className="details-container">
-                    <p></p>
+                    <p>Sales App</p>
                     <img src="/img/sales_app.png" alt="" />
                     <button onClick={()=>{
 
@@ -33,7 +33,7 @@ const Home = ()=>{
                     }}>Live Demo</button>
                 </div>
                 <div className="details-container">
-                    <p></p>
+                    <p>Imagen</p>
                     <img src="/img/Imagen_landingPage.png" alt="" />
                     <button onClick={()=>{
                         window.location.href = 'https://www.figma.com/proto/ryn0xjKHHf1Xyq3dZPAGxc/website_banner?page-id=0%3A1&node-id=3-2&t=ldA8beE1v7mVq7ck-1'
@@ -47,9 +47,13 @@ const Home = ()=>{
                     }}>Live Demo</button>
                 </div>
                 
-               
-               
-                
+                <div className="details-container">
+                    <p>Burger King</p>
+                    <img src="/img/fruitCider_animation.png" alt="" />
+                    <button onClick={()=>{
+                        window.location.href = 'https://www.figma.com/proto/nw0KcoIp9eWgSxTy7Zy2fR/PlanCover?page-id=410%3A18&node-id=470-3&viewport=-121%2C-348%2C0.68&t=aKq5uQTyq9NPsNUk-1&scaling=min-zoom&content-scaling=fixed'
+                    }}>Live Demo</button>
+                </div>
             </div>
         </div>
     )
