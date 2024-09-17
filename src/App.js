001dca744ff30bcd1509168a14b6954b17598164
Abhinav Arora <abhinavarora310@gmail.com>
2024-09-18 01:04:52 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 import Home from './Home';
+
 function App() {
+  const [showHome, setShowHome] = useState(false);
+
+  const handleVideoEnd = () => {
+    setShowHome(true); 
+  };
+
   return (
     <div className="App">
-      <Home></Home>
+      {showHome ? (
+        <Home />
+      ) : (
+       <div className='intro-video-container'>
+          <video
+            src="./img/Splash_Video.mp4"
+            className="intro-video"
+            // width={"500px"}
+            // height={"500px"}
+            autoPlay
+            muted
+            onEnded={handleVideoEnd}
+          />
+       </div>
+      )}
     </div>
   );
 }
