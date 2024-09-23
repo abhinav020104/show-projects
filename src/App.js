5e9a1b078b0a29d2487c689cd6566a72ef67b884
Abhinav Arora <abhinavarora310@gmail.com>
2024-09-23 21:16:20 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import Home from './Home';
-import { useRef } from 'react';
+
 function App() {
   const [showHome, setShowHome] = useState(false);
   const [videoUrl, setVideoUrl] = useState('./img/splash_video_landscape.mp4');
-  const videoRef = useRef(null)
+  const videoRef = useRef(null);
+
   useEffect(() => {
     const handleResize = () => {
-      if (videoRef.current) {
-        videoRef.current.playbackRate = 1.5;
-      }
       if (window.innerWidth < 500) {
-        setVideoUrl('./img/splash_video_portrait.mp4'); // Replace with your mobile video URL
-      }else {
-        setVideoUrl('./img/splash_video_landscape.mp4'); // Replace with your landscape video URL
+        setVideoUrl('./img/splash_video_portrait.mp4'); // Use portrait video for mobile
+      } else {
+        setVideoUrl('./img/splash_video_landscape.mp4'); // Use landscape video for larger screens
       }
     };
 
-    window.addEventListener('resize', handleResize);
+    // Set video URL initially and on resize
     handleResize();
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', handleResize); // Cleanup listener
     };
   }, []);
 
+  const handleLoadedMetadata = () => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 1.5; // Set playback speed
+    }
+  };
+
   const handleVideoEnd = () => {
-    setShowHome(true);
+    setShowHome(true); // Navigate to Home after the video ends
   };
 
   return (
@@ -43,6 +48,7 @@ function App() {
             autoPlay
             muted
             onEnded={handleVideoEnd}
+            onLoadedMetadata={handleLoadedMetadata} // Set playback speed after metadata is loaded
           />
         </div>
       )}
